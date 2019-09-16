95033f6e66d0e0301684970e312cbd278062b998
Jon Preece <jonpreece@hotmail.co.uk>
2019-09-16 20:38:20 +0100

    Added script to generate TS type definitions

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { About } from "./pages/About"
 import { NotFound } from "./pages/NotFound"
 import { Layout } from "./components/Layout"
 
-import "./global/styles"
+require("./global/styles.css")
 
 if (module.hot) {
   module.hot.accept()
